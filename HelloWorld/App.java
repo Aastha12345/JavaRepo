72dff1ae2bd26758369473ce49c93ef7704c5cc4
riya <riyaparuthi@gmail.com>
2024-10-06 20:10:45 +0530

    Base repo setup

---
+++ HelloWorld/App.java
@@ -0,0 +1,15 @@
+// This is a simple Java program.
+// FileName : "HelloWorld.java".
+
+import com.example.utils.Utility;
+
+class App {
+    // Your program begins with a call to main().
+    // Prints "Hello, World" to the terminal window.
+    public static void main(String args[])
+    {
+        Utility utility = new Utility();
+        utility.sum(5, 6);
+        System.out.println("Hello, World");
+    }
+}
---
+++ HelloWorld/com/example/utils/Utility.java
@@ -0,0 +1,9 @@
+package com.example.utils;
+
+public class Utility {
+    public int sum(int a, int b)
+    {
+        System.out.println("The sum of a and b");
+        return a + b;
+    }
+}
